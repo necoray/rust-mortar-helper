685aa356e568dc849fa2a3e5a2518ce5e275c4f1
necoray <91852858+necoray@users.noreply.github.com>
2026-05-17 12:52:21 +0000

    feat(fire-offset): add import format intervals

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { mortarState } from './state.js';
 import { initAnalysis, addShotRow } from './features/analysis.js';
 import { initAiming } from './features/aiming.js';
 import { drawChart } from './chart.js';
-import { formatPolynomial, getScatterIntervals } from './math.js';
+import { formatPolynomial, getScatterIntervals, formatInterval } from './math.js';
 
 const DEFAULT_DATA = {
   "v": "1.0",
